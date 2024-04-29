5d78bbdcabaf62dc47f8e66cd4b106ea1427affa
Lou Vaughn <ljvaugh0841@eagle.fgcu.edu>
2024-04-29 15:03:22 -0400

    Scrolling Sprite Ver 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,20 @@
 import React, { useState, useEffect } from 'react';
 import { gsap } from 'gsap';
 import "../index.css";
+import SpriteSheet from "react-responsive-spritesheet"
 
 const Hero = () => {
 
-    function scrollCallback() {
+    const totalFrames = 15;
 
-    }
+    var spritesheetInstance = null;
+
+    const scrollCallback = () => {
+        const position = window.scrollY;
+        var frameIndex = Math.floor(position / (window.innerHeight / totalFrames) + 1);
+        console.log(frameIndex);
+        spritesheetInstance.goToAndPause(frameIndex)
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', scrollCallback);
@@ -21,8 +29,20 @@ const Hero = () => {
 
     return (
         <div className="hero-section border border-yellow-400">
-            <div className="sprite sprite-1">
-
+            <div className="sprite sprite-1 w-full justify-center">
+            <SpriteSheet
+                image={`src/assets/desktop_spritesheet.png`}
+                widthFrame={200}
+                heightFrame={150}
+                steps={15}
+                fps={10}
+                autoplay={false}
+                loop={false}
+                isResponsive={true}
+                getInstance={spritesheet => {
+                    spritesheetInstance = spritesheet;
+                  }}
+            />
             </div>
         </div>
     )
