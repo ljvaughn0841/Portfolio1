2bf0c465941d7b4a79d903bf40aea01564f71d7e
Lou Vaughn <ljvaugh0841@eagle.fgcu.edu>
2024-05-02 11:24:19 -0400

    Parrallax Scroll
     * Testing a basic parallax scrolling for the hero section.
     * Just adding parallax scroll to the hero section alone causes some scrolling issues ( basically its another scroll box inside the website rather than being cohesive )

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { gsap } from 'gsap';
 import "../index.css";
 import SpriteSheet from "react-responsive-spritesheet"
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 
 const Hero = () => {
 
@@ -13,6 +14,7 @@ const Hero = () => {
 
     const scrollCallback = () => {
         const position = window.scrollY;
+        // add an if statement to skip all this after reaching a certain point in the page
         var frameIndex = Math.floor(position / (window.innerHeight / totalFrames) + 1);
         console.log(frameIndex);
         spritesheetInstance.goToAndPause(frameIndex)
@@ -27,23 +29,41 @@ const Hero = () => {
         };
       }, []);
 
+      // somehow add paralax scrolling :) and position the items right and make mobile freindly
     return (
         <div className="hero-section border border-yellow-400">
-            <div className="sprite sprite-1 w-full justify-center">
-            <SpriteSheet
-                image={`src/assets/desktop_spritesheet.png`}
-                widthFrame={200}
-                heightFrame={150}
-                steps={15}
-                fps={10}
-                autoplay={false}
-                loop={false}
-                isResponsive={true}
-                getInstance={spritesheet => {
-                    spritesheetInstance = spritesheet;
-                  }}
-            />
-            </div>
+            <Parallax pages={2} className='parallax-remove-scrollbar'>
+                <ParallaxLayer offset={0} speed={0.1}>
+                <SpriteSheet
+                            image={`src/assets/spritesheet.png`}
+                            widthFrame={70}
+                            heightFrame={50}
+                            steps={10}
+                            fps={10}
+                            autoplay={true}
+                            loop={true}
+                            isResponsive={true}
+                            />
+                </ParallaxLayer>
+                <ParallaxLayer offset={0.6} speed={2.5}>
+                    <div className="sprite sprite-1 w-full justify-center">
+                        <SpriteSheet
+                            image={`src/assets/desktop_spritesheet.png`}
+                            widthFrame={200}
+                            heightFrame={150}
+                            steps={15}
+                            fps={10}
+                            autoplay={false}
+                            loop={false}
+                            isResponsive={true}
+                            getInstance={spritesheet => {
+                                spritesheetInstance = spritesheet;
+                            }}
+                        />
+                        </div>
+                </ParallaxLayer>
+            </Parallax>
+            
         </div>
     )
 }
