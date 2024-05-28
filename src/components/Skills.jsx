a757faf65fb2c0edf51df4e632f5f15410e49eda
Lou Vaughn <ljvaugh0841@eagle.fgcu.edu>
2024-05-28 16:18:21 -0400

    Fix Bug
     - fixed an issue where the body would shrink at a low resolution (creating a bar on the right side of the page) because of elements in the skills section having a greater width than the mobile view's width.
     - Also reformatted the skills box spritesheet to remove white space.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,19 +11,19 @@ import "../index.css";
 //      2. Make div always a multiple of the sprite sheet to scale well
 const Skills = () => {
     return (
-        <div className="skills-section mt-10 border border-green-500">
+        <div className="skills-section mt-10 border border-green-500 overflow-x-hidden">
             <div className="flex flex-wrap gap-10 mb-10 justify-center">
                 {mySkills.map((item) => (
-                    <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] border border-orange-700"
+                    <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] max-w-[24rem] border border-orange-700"
                     key={item.id}>
-                        <div className="relative z-20 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                        <div className="relative z-20 flex flex-col min-h-[22rem] p-[3rem] pointer-events-none">
                             <h5 className="mb-5">{item.title}</h5>
                             <p className="mb-6">{item.text}</p>
                         </div>
-                        <div className="absolute top-0 left-0 w-full h-full z-10">
+                        <div className="absolute top-0 left-0 w-full h-full z-10 scale-110">
                         <SpriteSheet
                             image={`src/assets/spritesheet.png`}
-                            widthFrame={70}
+                            widthFrame={50}
                             heightFrame={50}
                             steps={10}
                             fps={10}
