eceb44962cdb9097f1293b7497ea43d564cf3593
Lou Vaughn <ljvaughn0841@gmail.com>
2025-09-26 01:02:22 -0400

    Added Mobile Menu
    - Added a temporary mobile menu.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,12 @@ import  {Link} from 'react-router-dom';
 
 //import { styles } from '../styles';
 import { navLinks } from '../constants';
+import menu from "../assets/menu.svg";
+import close from "../assets/close.svg";
+
 const NavBar = () => {
     const [active, setActive] = useState('')
+    const [toggle, setToggle] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -45,8 +49,30 @@ const NavBar = () => {
 
             {/* Mobile Nav Bar */}
             <div className="sm:hidden flex flex-1 justify-between items-center mx-5">
-                <p>NAME/LOGO PLACEHOLDER</p>
-                <p>MENU ICON</p>
+                <div className='sm:hidden flex flex-1 justify-start items-center'>
+                    {/* <img src='src/assets/ljv_logo.png' alt="logo" className="w-9 h-9 object-contain"/> */}
+                    <p className='ml-5 silkscreen-bold text-xl'>LJV</p>
+                </div>
+                <img src={toggle ? close : menu} alt="logo" className="w-9 h-9 object-contain" onClick={() => setToggle((prev) => !prev)}/>
+
+                <div className={`${!toggle ? 'hidden' : 'flex'} absolute top-[78px] right-0 z-10 rounded-xl`}>
+                    <ul className="list-none flex justify-end items-start flex-col gap-5 tiny5-regular bg-tertiary pr-10 pl-5 pt-2 pb-2">
+                        {navLinks.map((Link) => (
+                    <li key={Link}
+                    className={`${
+                        active === Link.title
+                        ? "text-white"
+                        : "text-secondary"
+                    } hover:text-[#67637c] text-[20px] font-medium cursor-pointer
+                    `}
+                    onClick={() => setActive(Link.title)}
+                    >
+                        <a href={`#${Link.id}`}>{Link.title}</a>
+                    </li>
+                ))}
+            </ul>
+
+                </div>
             </div>
 
         </div>
